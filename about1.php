179df2ab84ad9a4558a20634894b6c7e51004e61
issc-Swapnil <patilswapnil8956@gmail.com>
2025-03-27 20:47:26 +0530

    converted into php

---
+++ about1.php
@@ -0,0 +1,28 @@
+<?php 
+include 'header.php'; 
+?>
+<div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container">
+    <div class="responsive-container-block leftSide">
+      <p class="text-blk heading">
+        About Us
+      </p>
+      <p class="text-blk subHeading">
+      RDIGS was founded by a group of individuals who all came from sales and B2B lead generation backgrounds and brought 15+ years of experience to the table. They noticed a significant gap in the market and were determined to eliminate it. Despite being technology- driven, the company values the importance of human connection and ensures every client receives a personalized experience. At RDIGS, we pride ourselves on our ability to bridge the gap between technology and human interaction to provide exceptional lead generation services.
+      </p>
+    </div>
+    <div class="responsive-container-block rightSide">
+      <img class="number1img" src="img/pageabout1.png">
+      <img class="number2img" src="img/pageabout2.png">
+      <img class="number3img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/b245.png">
+      <img class="number5img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Customer supports.png">
+      <iframe allowfullscreen="allowfullscreen" class="number4vid" poster="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/b242.png"">
+      </iframe>
+      <img class="number7img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/d51.png">
+      <img class="number6img" src="pageabout4.webp">
+    </div>
+  </div>
+</div>
+<?php 
+include 'footer.php'; 
+?>
---
+++ service.php
@@ -0,0 +1,195 @@
+<?php 
+include 'header.php'; 
+?>
+
+
+        <!-- Service Start -->
+        <div class="container-fluid service py-5">
+            <div class="container py-5">
+                <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
+                    <h4 class="text-primary">Our Services</h4>
+                    <h1 class="display-4 mb-4">We Provide Best Services</h1>
+                    <p class="mb-0">RDIGS is your strategic partner in driving unstoppable B2B growth with data-powered marketing and sales solutions. Our expertise in account-based marketing, intent-driven strategies, and precision sales development fuels high-quality lead generation and revenue acceleration. We craft intelligent, results-driven campaigns that amplify engagement, maximize conversions, and position your brand ahead of the competition. From content syndication to cutting-edge digital marketing, our unrivaled approach ensures sustainable success. Unlock new business opportunities and supercharge your sales with RDIGS—where innovation meets impact.
+                    </p>
+                </div>
+                <div class="row g-4 justify-content-center pt-5">
+                    <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="service-item">
+                            <div class="service-img">
+                                <img src="img/b3b lead.webp" class="img-fluid rounded-top w-100" alt="">
+                                <!-- <div class="service-icon p-3">
+                                    <i class="fa fa-users fa-2x"></i>
+                                </div> -->
+                            </div>
+                            <div class="service-content p-4">
+                                <div class="service-content-inner">
+                                    <a href="#" class="d-inline-block h4 mb-4">B2B Lead Generation </a>
+                                    <p class="mb-4">
+                                     Empower Your Sales Team and Drive Results with RDIGS’ Account-Based Marketing Services
+                                    </p>
+                                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="service-item">
+                            <div class="service-img">
+                                <img src="img/b2badve.jpeg" class="img-fluid rounded-top w-100" alt="">
+                                <!-- <div class="service-icon p-3">
+                                    <i class="fa fa-hospital fa-2x"></i>
+                                </div> -->
+                            </div>
+                            <div class="service-content p-4">
+                                <div class="service-content-inner">
+                                    <a href="#" class="d-inline-block h4 mb-4">B2B Advertising</a>
+                                    <p class="mb-4">Drive Sustainable Growth for Your Sales and Marketing Efforts with Data-Driven Strategies
+                                    </p>
+                                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="service-item">
+                            <div class="service-img">
+                                <img src="img/sdr.webp" class="img-fluid rounded-top h-100 w-100" alt="">
+                                <!-- <div class="service-icon p-3">
+                                    <i class="fa fa-car fa-2x"></i>
+                                </div> -->
+                            </div>
+                            <div class="service-content p-4">
+                                <div class="service-content-inner">
+                                    <a href="#" class="d-inline-block h4 mb-4">B2B SDR as a Service</a>
+                                    <p class="mb-4">Unlock the Power of Precision Marketing with RDIGS’ Intent-Based Marketing Services
+                                   </p>
+                                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.8s">
+                        <div class="service-item">
+                            <div class="service-img">
+                                <img src="img/consy.png" class="img-fluid rounded-top w-100" alt="">
+                                <!-- <div class="service-icon p-3">
+                                    <i class="fa fa-home fa-2x"></i>
+                                </div> -->
+                            </div>
+                            <div class="service-content p-4">
+                                <div class="service-content-inner">
+                                    <a href="#" class="d-inline-block h4 mb-4">Content Syndication</a>
+                                    <p class="mb-4">Now is the time to Propel Your B2B Growth & Boost Your Sales Performance with RDIGS Sales</p>
+                                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.2s">
+                        <a class="btn btn-primary rounded-pill py-3 px-5" href="#">More Services</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Service End -->
+
+
+        <!-- Testimonial Start -->
+        <div class="container-fluid testimonial pb-5">
+            <div class="container pb-5">
+                <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
+                    <h4 class="text-primary">Testimonial</h4>
+                    <h1 class="display-4 mb-4">What Our Customers Are Saying</h1>
+                    <p class="mb-0">Trusted Partnerships, Proven Results: Hear from Our Satisfied Clients
+                        Our commitment to excellence drives success for businesses worldwide. From high-quality leads to unmatched responsiveness, RDIGS empowers brands to achieve their sales and marketing goals. Don’t just take our word for it—see what our clients have to say!
+                    </p>
+                </div>
+                <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="testimonial-item bg-light rounded">
+                        <div class="row g-0">
+                            <!-- <div class="col-4  col-lg-4 col-xl-3">
+                                <div class="h-100">
+                                    <img src="img/Taas.webp" class="img-fluid h-10 rounded" style="object-fit: cover;" alt="logo">
+                                </div>
+                            </div> -->
+                            <div class="col-8 col-lg-8 col-xl-9">
+                                <div class="d-flex flex-column my-auto text-start p-4">
+                                    <div class="col-4  col-lg-4 col-xl-3">
+                                        <div class="h-100">
+                                            <img src="img/Taas.webp" class="img-fluid h-10 rounded" style="object-fit: cover;" alt="logo">
+                                        </div>
+                                    </div>
+                                    <h4 class="text-dark mb-0"> Sarah Miller</h4>
+                                    <p class="mb-3">Marketing Director, Global Brands</p>
+                                    <div class="d-flex text-primary mb-3">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                    </div>
+                                    <p class="mb-0">"We have worked with RDIGS as our delivery partner for over three years. Throughout the period they have provided amazing high-quality leads along with responsiveness to both planned, unplanned, and urgent needs. They have developed a good set of capabilities in their team and processes to ensure that their delivery is timely and comes with uncompromised quality. All in all, they are a good team and willing to go the extra mile when needed to partner in the client's success.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-light rounded">
+                        <div class="row g-0">
+                            <div class="col-8 col-lg-8 col-xl-9">
+                                <div class="d-flex flex-column my-auto text-start p-4">
+                                    <div class="col-4  col-lg-4 col-xl-3">
+                                        <div class="h-100">
+                                            <img src="img/wholehouse3.webp" class="img-fluid h-10 rounded" style="object-fit: cover;" alt="">
+                                        </div>
+                                    </div>
+                                    <h4 class="text-dark mb-0">Emily Rodriguez</h4>
+                                    <p class="mb-3">Operations Director, Enterprise Solutions</p>
+                                    <div class="d-flex text-primary mb-3">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star text-body"></i>
+                                    </div>
+                                    <p class="mb-0">"We have worked with RDIGS as our delivery partner for over three years. Throughout the period they have provided amazing high-quality leads along with responsiveness to both planned, unplanned, and urgent needs. They have developed a good set of capabilities in their team and processes to ensure that their delivery is timely and comes with uncompromised quality. All in all, they are a good team and willing to go the extra mile when needed to partner in the client's success.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-light rounded">
+                        <div class="row g-0">
+                            <div class="col-8 col-lg-8 col-xl-9">
+                                <div class="d-flex flex-column my-auto text-start p-4">
+                                    <div class="col-4  col-lg-4 col-xl-3">
+                                        <div class="h-100">
+                                            <img src="img/Goto.webp" class="img-fluid h-110 rounded" style="object-fit: cover;" alt="">
+                                        </div>
+                                    </div>
+                                    <h4 class="text-dark mb-0">John Deo</h4>
+                                    <p class="mb-3">CEO, Innovation Tech</p>
+                                    <div class="d-flex text-primary mb-3">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star text-body"></i>
+                                        <i class="fas fa-star text-body"></i>
+                                    </div>
+                                    <p class="mb-0">"Thanks to RDIGS' incredible work, timely leads, and flexibility, especially since you guys help us achieve our demand gen goals each quarter. Our executive team can now see an exemplary Opportunity
+                                        to Close/Win rate along with solid ROI. We are hoping to grow this partnership with RDIGS throughout!"
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+
+
+        <!-- Footer Start -->
+        <?php 
+include 'footer.php'; 
+?>
